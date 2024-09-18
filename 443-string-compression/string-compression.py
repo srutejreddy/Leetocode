9c322f453e40d8ca39de5321ca3042f4bc859617
Srutej Reddy <53833953+srutejreddy@users.noreply.github.com>
2024-09-18 12:40:36 -0700

    Time: 63 ms (23.46%) | Memory: 16.7 MB (52.07%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,31 +1,20 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        def addCountToChars(idx, count, chars):
-            if count == 1:
-                return idx
-            elif count < 10:
-                chars[idx] = str(count)
-                idx+=1
-            else:
-                for s in str(count):
-                    chars[idx] = s
-                    idx+=1
-            return idx
-
+        slow, fast = 0, 0
         N = len(chars)
-        currChar = chars[0]
-        count = 1
-        idx = 0
-        for i in range(1,N):
-            if chars[i] == currChar:
+
+        while fast < N:
+            currChar = chars[fast]
+            count = 0
+
+            while fast<N and chars[fast] == currChar:
+                fast+=1
                 count+=1
-            else:
-                chars[idx] = currChar
-                idx+=1
-                idx = addCountToChars(idx, count, chars)
-                currChar = chars[i]
-                count = 1
-        chars[idx] = currChar
-        idx+=1
-        idx = addCountToChars(idx,count,chars)
-        return idx
+            
+            chars[slow] = currChar
+            slow+=1
+            if count>1:
+                for s in str(count):
+                    chars[slow] = s
+                    slow+=1
+        return slow
