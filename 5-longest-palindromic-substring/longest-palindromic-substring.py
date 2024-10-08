c88b2835540e42c48f9cab47b4dd9a0be4bf76de
Srutej Reddy <53833953+srutejreddy@users.noreply.github.com>
2024-10-08 17:22:58 -0400

    Time: 239 ms (93.17%) | Memory: 16.7 MB (25.10%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -7,15 +7,15 @@ class Solution:
             while l>=0 and r<N and s[l]==s[r]:
                 l-=1
                 r+=1
-            return l+1, r
+            return l+1, r-1
 
         for i in range(N):
             l, r = LP(i, i)
-            if r-l > end-start:
+            if r-l+1 > end-start+1:
                 start, end = l, r
             
             l, r = LP(i, i+1)
-            if r-l > end-start:
+            if r-l+1 > end-start+1:
                 start, end = l,r
-
-        return s[start: end]
+                
+        return s[start: end+1]
