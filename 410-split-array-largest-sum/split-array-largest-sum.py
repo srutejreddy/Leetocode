a111f01ec141f5f68897c6f064baa3b70126e792
Srutej Reddy <53833953+srutejreddy@users.noreply.github.com>
2024-10-10 15:43:24 -0400

    Time: 35 ms (89.64%) | Memory: 16.7 MB (27.86%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        def feasible(threshold):
+            count = 1
+            total = 0
+            for num in nums:
+                total+=num
+                if total>threshold:
+                    total = num
+                    count+=1 
+                    if count > k:
+                        return False
+            return True
+        
+        left, right = max(nums), sum(nums)
+        while left<right:
+            mid = left + (right-left)//2
+            if feasible(mid):
+                right = mid
+            else:
+                left = mid+1
+        return left
+        
